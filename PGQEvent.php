dd1ecdd4702f6448ae08f0c440eee6e4b5a88ac0
dim <dim>
2008-07-02 10:48:16 +0000

    Add failed_reason and failed_time to PGQEvent and dispay them

--- PGQEvent.php
+++ PGQEvent.php
@@ -32,13 +32,15 @@ class PGQEvent
    * read C logutriga code in Skytools module for how to map
    */
   public function init_from_row($row) {
-    $this->id    = (int)$row["ev_id"];
-    $this->time  = $row["ev_time"];
-    $this->txid  = $row["ev_txid"];
-    $this->retry = $row["ev_retry"];
-    $this->type  = $row["ev_type"];
-    $this->data  = $this->decode($row["ev_data"]);
-    $this->table = $row["ev_extra1"];
+    $this->id            = (int)$row["ev_id"];
+    $this->time          = $row["ev_time"];
+    $this->txid          = $row["ev_txid"];
+    $this->retry         = $row["ev_retry"];
+    $this->type          = $row["ev_type"];
+    $this->data          = $this->decode($row["ev_data"]);
+    $this->table         = $row["ev_extra1"];
+    $this->failed_reason = $row[ "ev_failed_reason" ];
+    $this->failed_time   = $row[ "ev_failed_time" ];
   }
 
   /**
@@ -81,11 +83,20 @@ class PGQEvent
    * String representation
    */
   public function __toString() {
-    return sprintf("%10d: %s %s %s", 
+    $data_str = "";
+    
+    foreach( $this->data as $fieldName => $value ) {
+	$data_str .= "\n\t[".$fieldName."] => ".$value;
+    }
+
+    return sprintf("Event id : %10d\nTime : %s\nFailed time : %s\n".
+		   "Type: %s\nFailed reason : %s\nData :%s\n", 
 		   $this->id, 
 		   $this->time,
+		   $this->failed_time,
 		   $this->type,
-		   print_r($this->data, True));
+		   $this->failed_reason,
+		   $data_str );
   }
 
   /**
