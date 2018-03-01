1d7da4405da4459fb22d7cb304faac9a5773bdf1
wapmorgan <wapmorgan@gmail.com>
2018-03-01 17:03:23 +0300

    Update config

--- PGQ.php
+++ PGQ.php
@@ -9,13 +9,6 @@
  *  $batch_id = PGQ::next_batch();
  */
 
-// No sense is given nor necessary for those constants, as soon as
-// there's no colision.
-define("PGQ_EVENT_OK",     1);
-define("PGQ_EVENT_FAILED", 2);
-define("PGQ_EVENT_RETRY",  5);
-define("PGQ_ABORT_BATCH", 11);
-
 abstract class PGQ {
   /**
    * Queue creation
